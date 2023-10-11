6a53fb954a86e03004921acf97cf2d372ff91ac9
jeonminji04 <jeonminji04@g.shingu.ac.kr>
2023-10-11 10:59:12 +0900

    Feat : "DB_Connect.JS 추가"

---
+++ public/DB_Connect.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : process.env.DB_HOST,
+    port : process.env.DB_PORT,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME,
+});
+
+connection.connect((err)=> {
+
+    if(err)
+    {
+        console.error("MYSQL 연결 오류 : " + err.stack);
+        return;
+    }
+
+    console.log("연결 되었습니다. 연결 ID : " + connection.threadId);
+
+});
+
+connection.query('SELECT * FROM userid', (err, results, fileds) => {
+    
+    if(err) throw err;
+
+    const dataArray = results;
+
+    console.log('데이터 배열 : ', dataArray[0].index);
+
+});  
+
+connection.end((err) => {
+    if(err){
+        console.error('MYSQL 연결 종료 오류 : ' + err.stack);
+        return;
+    }
+    console.log("MYSQL 연결이 성공적으로 종료되었습니다.");
+});
